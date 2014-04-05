2191e3617420ef36d51d6b62b856636ece2d118f
Ismail Badawi <ismail@badawi.io>
2014-04-05 11:47:31 -0400

    Generate posts controller.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def create
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
