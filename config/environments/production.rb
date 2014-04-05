a9ce9de388b7f3a50c5b5c659165648acf8490b6
Ismail Badawi <ismail@badawi.io>
2014-04-05 10:37:03 -0400

    Add basic authentication via devise.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Forum::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = {
+    host: 'dry-falls-4447.herokuapp.com'
+  }
 end
