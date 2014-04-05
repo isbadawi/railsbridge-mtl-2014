8821ea9336822917d365257167e8f8d7a58b2d59
Ismail Badawi <ismail@badawi.io>
2014-04-05 09:58:25 -0400

    Initial commit + hello world controller.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Forum::Application.config.secret_key_base = '043cda5629ce678002481017c636d0ca5eba0cb537d994b1d2b0395741ffb2a7b3825c5d9ec78cf8587c9144704e029b29515fa6eb7045ee7748b0b097ac4bef'
