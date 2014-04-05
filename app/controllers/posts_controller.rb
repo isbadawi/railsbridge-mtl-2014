0ef6054397485afd2a4ed9b0caca1ada59a7bfa6
Ismail Badawi <ismail@badawi.io>
2014-04-05 13:27:06 -0400

    Allow adding new posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
   def create
+    @post = Post.create(post_params.merge(user: current_user))
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = @post.errors.full_messages.join('<br/>')
+      render template: 'posts/new'
+    end
   end
 
   def edit
@@ -9,14 +16,22 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
